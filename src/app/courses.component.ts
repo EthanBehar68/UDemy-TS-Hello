177e128b0a94996b97f66d04c0616eeca9921977
Ethan Behar <behar.ethan@gmail.com>
2020-03-18 09:04:37 -0400

    playing with directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,8 +4,17 @@ import { Component } from '@angular/core'
 //Decorator Function
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>' // HTML mark up 
+    // HTML mark up
+    template: `
+    <h2>{{ title }}</h2>
+    <ul>
+        <li *ngFor="let course of courses">
+            {{course}}
+        </li>
+    </ul>
+    `
 })
 export class CoursesComponent {
-    
+    title = "List of courses";
+    courses = ["course1", "course2", "course3"]
 }
