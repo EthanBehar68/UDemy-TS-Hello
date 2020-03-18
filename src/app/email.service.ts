7e3795bd22d6decaaae5056527354fd0247b9563
Ethan Behar <behar.ethan@gmail.com>
2020-03-18 09:19:46 -0400

    comment on Injectable

--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@angular/core';
+
+// Component already includes Injectable keyword 
+// So courses service didn't need it
+import { Injectable } from '@angular/core'; 
+
 
 @Injectable({
   providedIn: 'root'
